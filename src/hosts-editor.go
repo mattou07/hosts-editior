d70abe25df13613c76e8ea8a9b5ca055d67cc825
Matthew Hart <matthew258@gmail.com>
2019-11-24 22:14:29 +0000

    Working list entries commandline flag

--- src/hosts-editor.go
+++ src/hosts-editor.go
@@ -1,7 +1,15 @@
+/*
+Proposed Commandline flags to be used:
+-h -help prints out a list of arguments and what they do
+-l -list lists out all the host entries currently in the host file
+-a -append [ip] [hostname] Example: -a 127.0.0.1 mattou07.local
+-d -delete -del [hostname] Delete an entry in the hosts file
+*/
 package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"regexp"
@@ -19,25 +27,41 @@ func check(e error) {
 	}
 }
 
-//TODO handle lines with empty space
 func parseEntry(ent string) entry {
 	ip := regexp.MustCompile(`(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}`)
 	spaces := regexp.MustCompile(`\s*`)
-	submatchall := ip.FindAllString(ent, -1)
+	matchedIP := ip.FindAllString(ent, -1)
 	ent = ip.ReplaceAllString(ent, "")
 	ent = spaces.ReplaceAllString(ent, "")
-	val := entry{ipAddress: submatchall[0], hostname: ent}
-	fmt.Println(val.ipAddress)
-	fmt.Println(val.hostname)
+	val := entry{ipAddress: matchedIP[0], hostname: ent}
 	return val
 }
 
-func main() {
-	argsWithProg := os.Args
-	argsWithoutProg := os.Args[1:]
+func list(entries []entry) {
+	fmt.Println("You have ", len(entries), " entries in your hosts file")
+	for _, ent := range entries {
+		fmt.Println("IP Address:", ent.ipAddress, "|", "Hostname:", ent.hostname)
+	}
+}
 
-	fmt.Println(argsWithProg)
-	fmt.Println(argsWithoutProg)
+func main() {
+	//Our commandline flags
+	var listArg bool
+	var delArg string
+	var appendArg string
+	flag.BoolVar(&listArg, "list", false, "Lists out all entries in the host file")
+	flag.BoolVar(&listArg, "l", false, "Lists out all entries in the host file")
+	flag.StringVar(&delArg, "delete", "", "Specifiy a host to be removed from the hosts file")
+	flag.StringVar(&delArg, "d", "", "Specifiy a host to be removed from the hosts file")
+	flag.StringVar(&appendArg, "append", "", "Specifiy a host and ip to be added to the hosts file")
+	flag.StringVar(&appendArg, "a", "", "Specifiy a host and ip to be added to the hosts file")
+
+	flag.Parse()
+
+	fmt.Println("List:", listArg)
+	fmt.Println("Delete:", delArg)
+	fmt.Println("Append:", appendArg)
+	fmt.Println("Tail:", flag.Args())
 
 	f, err := os.Open("E:/dev/go-lang/hosts-editior/src/hosts")
 	check(err)
@@ -49,10 +73,14 @@ func main() {
 	for scanner.Scan() {
 		if !strings.HasPrefix(scanner.Text(), "#") && scanner.Text() != "" {
 			//fmt.Println(scanner.Text())
-			parseEntry(scanner.Text())
-			item := entry{"", ""}
+
+			item := parseEntry(scanner.Text())
 			entries = append(entries, item)
 		}
 
+	}
+
+	if listArg {
+		list(entries)
 	}
 }
